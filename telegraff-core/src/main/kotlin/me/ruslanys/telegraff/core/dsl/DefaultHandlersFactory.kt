b701e6af97132a7d472ac33db4f14524d373d3bf
xzima <xzima@ro.ru>
2021-10-20 02:15:37 +0300

    refactor(core): remove kotlin-script logic form dsl

--- telegraff-core/src/main/kotlin/me/ruslanys/telegraff/core/dsl/DefaultHandlersFactory.kt
+++ telegraff-core/src/main/kotlin/me/ruslanys/telegraff/core/dsl/DefaultHandlersFactory.kt
@@ -15,65 +15,20 @@
  */
 package me.ruslanys.telegraff.core.dsl
 
-import org.jetbrains.kotlin.script.jsr223.KotlinJsr223JvmLocalScriptEngineFactory
-import org.jetbrains.kotlin.utils.addToStdlib.measureTimeMillisWithResult
 import org.slf4j.LoggerFactory
-import org.springframework.context.support.GenericApplicationContext
-import org.springframework.core.io.Resource
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver
-import org.springframework.stereotype.Component
-import javax.script.ScriptEngineFactory
 
-@Component
-class DefaultHandlersFactory(
-    private val context: GenericApplicationContext,
-    handlersPath: String
-) : HandlersFactory {
+class DefaultHandlersFactory(handlers: List<Handler>) : HandlersFactory {
 
-    private val resolver = PathMatchingResourcePatternResolver(javaClass.classLoader)
-    private val handlers: MutableMap<String, Handler> = hashMapOf()
+    private val handlerStorage: MutableMap<String, Handler> = hashMapOf()
 
     init {
-        val factory: ScriptEngineFactory = KotlinJsr223JvmLocalScriptEngineFactory()
-
-        val resources: Array<Resource> = try {
-            resolver.getResources("classpath:$handlersPath/*.kts")
-        } catch (e: Exception) {
-            log.warn("Can not load handlers by path `{}`: {}", handlersPath, e.message)
-            arrayOf()
-        }
-
-
-        for (resource in resources) {
-            val handler = try {
-                compile(factory, resource)
-            } catch (e: Exception) {
-                log.warn("Can not compile {}: {}", resource, e.message)
-                continue
-            }
-            addHandler(handler)
-        }
-    }
-
-    private fun compile(factory: ScriptEngineFactory, resource: Resource): Handler {
-        val scriptEngine = factory.scriptEngine
-
-        val compiled = measureTimeMillisWithResult {
-            resource.inputStream.bufferedReader().use {
-                @Suppress("UNCHECKED_CAST")
-                scriptEngine.eval(it) as HandlerDslWrapper
-            }
-        }
-
-        log.info("Compilation for ${resource.filename} took ${compiled.first} ms")
-
-        return compiled.second(context)
+        handlers.forEach { addHandler(it) }
     }
 
     private fun addHandler(handler: Handler) {
         for (rawCommand in handler.commands) {
             val command = rawCommand.lowercase()
-            val previousValue = handlers.put(command, handler)
+            val previousValue = handlerStorage.put(command, handler)
             if (previousValue != null) {
                 throw IllegalArgumentException("$command is already in use.")
             }
@@ -81,7 +36,7 @@ class DefaultHandlersFactory(
     }
 
     override fun getHandlers(): Map<String, Handler> {
-        return handlers
+        return handlerStorage
     }
 
     companion object {
--- telegraff-core/src/main/kotlin/me/ruslanys/telegraff/core/dsl/HandlerDsl.kt
+++ telegraff-core/src/main/kotlin/me/ruslanys/telegraff/core/dsl/HandlerDsl.kt
@@ -17,19 +17,18 @@ package me.ruslanys.telegraff.core.dsl
 
 import me.ruslanys.telegraff.core.dto.request.TelegramSendRequest
 import me.ruslanys.telegraff.core.exception.HandlerException
-import org.springframework.context.support.GenericApplicationContext
 
 
 fun handler(vararg commands: String, init: HandlerDsl.() -> Unit): HandlerDslWrapper {
-    return { context ->
-        val dsl = HandlerDsl(commands.asList(), context)
+    return {
+        val dsl = HandlerDsl(commands.asList())
         init(dsl) // handler.init()
 
         dsl.build()
     }
 }
 
-class HandlerDsl(private val commands: List<String>, val context: GenericApplicationContext) {
+class HandlerDsl(private val commands: List<String>) {
 
     private val stepDsls: MutableList<StepDsl<*>> = arrayListOf()
     private var process: ProcessBlock? = null
@@ -65,21 +64,12 @@ class HandlerDsl(private val commands: List<String>, val context: GenericApplica
 
 
         return Handler(
-                commands,
-                steps.associateBy { it.key },
-                stepDsls.firstOrNull()?.key,
-                process ?: throw HandlerException("Process block must not be null!")
+            commands,
+            steps.associateBy { it.key },
+            stepDsls.firstOrNull()?.key,
+            process ?: throw HandlerException("Process block must not be null!")
         )
     }
-
-    inline fun <reified T> getBean(): T {
-        return context.getBean(T::class.java)
-    }
-
-    fun readClasspathResource(path: String): ByteArray = javaClass.classLoader.getResourceAsStream(path).use {
-        it.readBytes()
-    }
-
 }
 
 class StepDsl<T : Any>(val key: String) {
@@ -104,13 +94,13 @@ class StepDsl<T : Any>(val key: String) {
 
     internal fun build(defaultNext: NextStepBlock): Step<T> {
         return Step(
-                key,
-                question ?: throw HandlerException("Step question must not be null!"),
-                validation ?: {
-                    @Suppress("UNCHECKED_CAST")
-                    it as T
-                },
-                next ?: defaultNext
+            key,
+            question ?: throw HandlerException("Step question must not be null!"),
+            validation ?: {
+                @Suppress("UNCHECKED_CAST")
+                it as T
+            },
+            next ?: defaultNext
         )
     }
 
@@ -122,4 +112,4 @@ typealias ProcessBlock = (state: HandlerState, answers: Map<String, Any>) -> Tel
 typealias QuestionBlock = (HandlerState) -> TelegramSendRequest
 typealias ValidationBlock<T> = (String) -> T
 typealias NextStepBlock = (HandlerState) -> String?
-typealias HandlerDslWrapper = (GenericApplicationContext) -> Handler
+typealias HandlerDslWrapper = () -> Handler
